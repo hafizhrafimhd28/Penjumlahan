f49073e5dfedf70c3feadd2368d4bbc49ca497da
Hafizh Rafi M <hafizhrafi28@gmail.com>
2021-03-30 09:05:41 +0700

    Perubahan pada print

--- penjumlahan.py
+++ penjumlahan.py
@@ -2,18 +2,18 @@ a = int(input("Masukkan Nilai 1: "))
 b = int(input("Masukkan Nilai 2: "))
 #Pertambahan
 c = a + b
-print("Hasil Pertambahan")
+print("Hasil Pertambahan :")
 print(c)
 #Pengurangan
 d = a - b
-print("Hasil Pengurangan")
+print("Hasil Pengurangan :")
 print(d)
 #Perkalian
 e = a * b
-print("Hasil Perkalian")
+print("Hasil Perkalian :")
 print(e)
 #Pembagian
 f = a / b
-print("Hasil Pembagian")
+print("Hasil Pembagian :")
 print(f)
       
